8d970ae0e5c17b413dce1fa5ebbcf238fd62dcdc
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2023-12-24 10:20:13 +0200

    modal  window mobile

--- src/js/modal.js
+++ src/js/modal.js
@@ -11,11 +11,26 @@ function toggleModal() {
   modal.classList.toggle('is-hidden');
 }
 
+const modalImgField = document.querySelector('#modal-img');
+const modalNameField = document.querySelector('#modal-name');
+const modalCategoryField = document.querySelector('#modal-category');
+const modalSizeField = document.querySelector('#modal-size');
+const modalPopularityField = document.querySelector('#modal-popularity');
+const modalDeskField = document.querySelector('#modal-desc');
+const modalPriceField = document.querySelector('#modal-price');
+
 export default async function showProductCard(id) {
   let response;
   try {
     response = await axios(BASE_URL + '/products/' + id);
     console.log(response);
+    modalImgField.src = response.data.img;
+    modalNameField.textContent = response.data.name;
+    modalCategoryField.textContent = response.data.category;
+    modalSizeField.textContent = response.data.size;
+    modalPopularityField.textContent = response.data.popularity;
+    modalDeskField.textContent = response.data.desc;
+    modalPriceField.textContent = '$' + response.data.price;
     toggleModal();
   } catch (error) {
     console.log(error);
--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import './js/carts';
+import showProductCard from './js/modal.js';
+const testModalBtn = document.querySelector('#test');
+testModalBtn.addEventListener('click', () =>
+  showProductCard('640c2dd963a319ea671e383b')
+);
