7750402da83f14d4f51f598081acea67f7294725
Albina Vivdenko <albinka3322@gmail.com>
2023-12-25 00:09:51 +0100

    css mob

--- src/js/discount.js
+++ src/js/discount.js
@@ -7,21 +7,23 @@ fetchDiscountProducts();
 async function fetchDiscountProducts() {
     const { data } = await axios.get("https://food-boutique.b.goit.study/api/products/discount");
 
-    const result = data.sort((a, b) => b.popularity - a.popularity).slice(0, 2);
- return result;
-
-}       
+//     const result = data.sort((a, b) => b.popularity - a.popularity).slice(0, 2);
+  //  return result;
+  const result = data.sort(() => Math.random() - 0.5).slice(0, 2);
+    return result;
+}
+     
 createDiscountMarkup();
 async function createDiscountMarkup() {
     const data = await fetchDiscountProducts();
-    const markup = data.map(({ img, is10PercentOff, price, name }) => `<li class="product-card"><img class="product-img" src="${img}" alt="${name}"/><div class="dis-card-description"><p class="dis-card-name">${name}</p><p class="dis-card-price">${price}</p><div class="carts-svg-box">
+    const markup = data.map(({ img, is10PercentOff, price, name }) => `<li class="dis-product-card"><img class="dis-product-img" src="${img}" alt="${name}"/><div class="dis-card-description"><p class="dis-card-name">${name}</p><p class="dis-card-price">$${price}</p><div class="carts-svg-box">
               <svg width="18" height="18">
                 <use href="./img/sprite.svg#icon-white-basket"></use>
               </svg>
             </div></div></li>`).join('');
     
     const container = document.createElement('div');
-    container.classList.add("container");
+    container.classList.add("dis-container");
 
     const title = document.createElement('h2');
     title.classList.add("discount-title");
