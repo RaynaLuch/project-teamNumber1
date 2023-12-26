3b5da4395f1e27ec87b56ec33f4c6f050077d9c5
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2023-12-26 15:31:44 +0000

    Deploying to gh-pages from @ RaynaLuch/project-teamNumber1@9a4fe49ba97598b949a7856e24b31130d700d306 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,25 +1,25 @@
-import"./assets/header-e8bb0568.js";const r=document.querySelector(".product-list"),i=document.querySelector(".sub-container"),p=[{img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e383b.png",productName:"Ackee",category:"Fresh_Produce",size:"16 oz",price:8.99},{img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3860.png",productName:"Allspice",category:"Pantry_Item",size:"1.5oz",price:2.99},{img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3861.png",productName:"Almond Extract",category:"Pantry_Items",size:"2oz",price:4.99},{img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e385e.png",productName:"Ancho Chillies",category:"Pantry_Items",size:"100g",price:4.99}];function n(){localStorage.setItem("basket",JSON.stringify(p))}n();function m(){const t=localStorage.getItem("basket");return JSON.parse(t)}const d=`<div class="empty">
+import"./assets/header-16e8538e.js";const p=document.querySelector(".product-list"),u=document.querySelector(".sub-container"),i=document.querySelector(".delete-btn"),c=document.querySelector(".count"),d=document.querySelector(".amount");function m(){let e,t=localStorage.getItem("basket");return t?e=JSON.parse(t):e=[],e}const g=`<div class="empty">
 <img src="./img/yellow shopping basket.png" alt="empty basket" class="empty-picture">
 <h2 class="empty-main-text">Your basket is <a href="../index.html" class="empty-link">empty...</a> </h2>
 <p class="empty-sub-text">Go to the main page to select your favorite products and add them to the cart.</p>
-</div>`;function g(){const t=localStorage.getItem("basket"),e=JSON.parse(t);if(console.log(e),e!==null){r.insertAdjacentHTML("beforeend",u(m()));return}i.innerHTML=d}g();function u(t){return t.map(({img:e,productName:s,category:a,size:c,price:o})=>`
-      <li class="product-cart">
+</div>`;function o(){const e=localStorage.getItem("basket"),t=JSON.parse(e);if(t!==null){const s=t.length;p.insertAdjacentHTML("beforeend",h(m())),c.textContent=s,d.textContent=b(t);return}c.textContent=0,u.innerHTML=g}o();i.addEventListener("click",y);function y(){localStorage.removeItem("basket"),o()}function b(e){let t=0;return e.map(({price:s})=>t+=s),t}function h(e){return e.map(({id:t,img:s,name:n,category:a,size:r,price:l})=>`
+      <li class="product-cart" data-id="${t}">
       <img
         class="product-picture"
-        src="${e}"
+        src="${s}"
         width="100"
         height="100"
       alt="avocado"
       />
       <div class="">
-        <p class="product-name">${s}</p>
+        <p class="product-name">${n}</p>
 
         <div class="product-descr">
           <p>Category: <span class="qwe">${a}</span></p>
-          <p>Size: <span class="qwe">${c}</span></p>
+          <p>Size: <span class="qwe">${r}</span></p>
         </div>
 
-        <p class="product-price">$${o}</p>
+        <p class="product-price">$${l}</p>
       </div>
       <button class="cart-btn" type="button">
         <svg class="">
