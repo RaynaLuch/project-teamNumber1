3702122fd1c8866117d22e4ee9f610a27e292807
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2023-12-27 07:53:55 +0000

    Deploying to gh-pages from @ RaynaLuch/project-teamNumber1@4f236ce8d755253c102d62ad11ed4bb1cd4268cc 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,31 +1,31 @@
-import"./assets/header-f86e9b7e.js";const l=document.querySelector(".product-list"),i=document.querySelector(".sub-container"),p=document.querySelector(".delete-btn"),o=document.querySelector(".count"),m=document.querySelector(".amount");function d(){let e,t=localStorage.getItem("basket");return t?e=JSON.parse(t):e=[],e}const g=`<div class="empty">
-<img src="/img/yellow shopping basket.png" alt="empty basket" class="empty-picture">
+import{r as i,b as u,g as p}from"./assets/manage-cart-9a051454.js";const n=document.querySelector(".product-list"),m=document.querySelector(".sub-container"),g=document.querySelector(".delete-btn"),a=document.querySelector(".count"),y=document.querySelector(".amount"),v=`<div class="empty">
+<img src="../img/yellow-shopping-basket.png" alt="empty basket" class="empty-picture">
 <h2 class="empty-main-text">Your basket is <a href="../index.html" class="empty-link">empty...</a> </h2>
 <p class="empty-sub-text">Go to the main page to select your favorite products and add them to the cart.</p>
-</div>`;function n(){const e=localStorage.getItem("basket"),t=JSON.parse(e);if(t!==null){const s=t.length;l.insertAdjacentHTML("beforeend",h(d())),o.textContent=s,m.textContent=b(t);return}o.textContent=0,i.innerHTML=g}n();p.addEventListener("click",y);function y(){localStorage.removeItem("basket"),n(),S()}function b(e){let t=0;return e.map(({price:s})=>t+=s),t}function h(e){return e.map(({id:t,img:s,name:c,category:a,size:r,price:u})=>`
+</div>`;function r(){const e=localStorage.getItem("basket"),t=JSON.parse(e);if(t!==null){const c=t.length;n.insertAdjacentHTML("beforeend",P(p())),a.textContent=c,y.textContent=f(t);return}a.textContent=0,m.innerHTML=v}r();n.addEventListener("click",b);function b(e){n.innerHTML="";const c=e.target.closest(".product-cart").dataset.id;i(c);const o=localStorage.getItem("basket"),s=JSON.parse(o);console.log(s.length),s.length===0&&u(),r()}g.addEventListener("click",h);function h(){u(),r()}function f(e){let t=0;return e.map(({price:c})=>t+=c),t.toFixed(2)}function P(e){return e.map(({_id:t,img:c,name:o,category:s,size:d,price:l})=>`
       <li class="product-cart" data-id="${t}">
       <img
         class="product-picture"
-        src="${s}"
+        src="${c}"
         width="100"
         height="100"
       alt="avocado"
       />
       <div class="">
-        <p class="product-name">${c}</p>
+        <p class="product-name">${o}</p>
 
         <div class="product-descr">
-          <p>Category: <span class="qwe">${a}</span></p>
-          <p>Size: <span class="qwe">${r}</span></p>
+          <p>Category: <span class="qwe">${s}</span></p>
+          <p>Size: <span class="qwe">${d}</span></p>
         </div>
 
-        <p class="product-price">$${u}</p>
+        <p class="product-price">$${l}</p>
       </div>
       <button class="cart-btn" type="button">
         <svg class="">
-          <use href="/img/sprite.svg#icon-cross"></use>
+          <use href="../img/sprite.svg#icon-cross"></use>
         </svg>
       </button>
     </li>
-    `).join("")}function S(){var s;const e=((s=JSON.parse(localStorage.getItem("basket")))==null?void 0:s.length)??0,t=document.querySelector(".itemsCount");t.innerText=e}
+    `).join("")}
 //# sourceMappingURL=commonHelpers2.js.map
