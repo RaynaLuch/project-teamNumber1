0559feb761d41afc0c4016a2f573530bffff33fe
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2023-12-27 08:43:10 +0000

    Deploying to gh-pages from @ RaynaLuch/project-teamNumber1@e46c744fd21eed8df984edbe81622b568080b8b2 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,8 +1,8 @@
-import{r as i,b as u,g as p}from"./assets/manage-cart-713b41ce.js";const r=document.querySelector(".product-list"),m=document.querySelector(".sub-container"),g=document.querySelector(".delete-btn"),a=document.querySelector(".count"),y=document.querySelector(".amount"),v=`<div class="empty">
-<img src="/src/img/yellow-shopping-basket.png" alt="empty basket" class="empty-picture">
+import{r as l,b as u,g as p,i as m}from"./assets/manage-cart-713b41ce.js";const n=document.querySelector(".product-list"),g=document.querySelector(".sub-container"),y=document.querySelector(".delete-btn"),a=document.querySelector(".count"),v=document.querySelector(".amount"),b=`<div class="empty">
+<img src="emptyBasketPicture" alt="empty basket" class="empty-picture">
 <h2 class="empty-main-text">Your basket is <a href="../index.html" class="empty-link">empty...</a> </h2>
 <p class="empty-sub-text">Go to the main page to select your favorite products and add them to the cart.</p>
-</div>`;function n(){const e=localStorage.getItem("basket"),t=JSON.parse(e);if(t!==null){const c=t.length;r.insertAdjacentHTML("beforeend",P(p())),a.textContent=c,y.textContent=f(t);return}a.textContent=0,m.innerHTML=v}n();r.addEventListener("click",b);function b(e){r.innerHTML="";const c=e.target.closest(".product-cart").dataset.id;i(c);const o=localStorage.getItem("basket"),s=JSON.parse(o);console.log(s.length),s.length===0&&u(),n()}g.addEventListener("click",h);function h(){u(),n()}function f(e){let t=0;return e.map(({price:c})=>t+=c),t.toFixed(2)}function P(e){return e.map(({_id:t,img:c,name:o,category:s,size:d,price:l})=>`
+</div>`;function r(){const e=localStorage.getItem("basket"),t=JSON.parse(e);if(t!==null){const c=t.length;n.insertAdjacentHTML("beforeend",k(p())),a.textContent=c,v.textContent=P(t);return}a.textContent=0,g.innerHTML=b}r();n.addEventListener("click",h);function h(e){n.innerHTML="";const c=e.target.closest(".product-cart").dataset.id;l(c);const o=localStorage.getItem("basket"),s=JSON.parse(o);console.log(s.length),s.length===0&&u(),r()}y.addEventListener("click",f);function f(){u(),r()}function P(e){let t=0;return e.map(({price:c})=>t+=c),t.toFixed(2)}function k(e){return e.map(({_id:t,img:c,name:o,category:s,size:d,price:i})=>`
       <li class="product-cart" data-id="${t}">
       <img
         class="product-picture"
@@ -19,11 +19,11 @@ import{r as i,b as u,g as p}from"./assets/manage-cart-713b41ce.js";const r=docum
           <p>Size: <span class="qwe">${d}</span></p>
         </div>
 
-        <p class="product-price">$${l}</p>
+        <p class="product-price">$${i}</p>
       </div>
       <button class="cart-btn" type="button">
         <svg class="">
-          <use href="/src/img/sprite.svg#icon-cross"></use>
+          <use href="${m}#icon-cross"></use>
         </svg>
       </button>
     </li>
