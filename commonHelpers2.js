907eb5e05727030692527c3182c339fe874c729d
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2023-12-28 09:14:02 +0000

    Deploying to gh-pages from @ RaynaLuch/project-teamNumber1@1a2e8567f660a7425fea221b7230bfbb579a1cf2 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,31 +1,31 @@
-import{r as i,b as u,g as p,i as m}from"./assets/manage-cart-52b4aebc.js";const g="/project-teamNumber1/assets/yellow-shopping-basket-88cc64a6.png",n=document.querySelector(".product-list"),y=document.querySelector(".sub-container"),b=document.querySelector(".delete-btn"),a=document.querySelector(".count"),h=document.querySelector(".amount"),v=`<div class="empty">
-<img src="${g}" alt="empty basket" class="empty-picture">
+import{r as m,b as u,g as d,i as g}from"./assets/modal-9c7f7af8.js";import{a as h}from"./assets/vendor-cb0d5946.js";const y="/project-teamNumber1/assets/yellow-shopping-basket-88cc64a6.png",r=document.querySelector(".product-list"),b=document.querySelector(".sub-container"),f=document.querySelector(".delete-btn"),a=document.querySelector(".count"),v=document.querySelector(".change"),S=document.querySelector(".order-form"),P="https://food-boutique.b.goit.study/api/orders",k=`<div class="empty">
+<img src="${y}" alt="empty basket" class="empty-picture">
 <h2 class="empty-main-text">Your basket is <a href="../index.html" class="empty-link">empty...</a> </h2>
 <p class="empty-sub-text">Go to the main page to select your favorite products and add them to the cart.</p>
-</div>`;function r(){const e=localStorage.getItem("basket"),t=JSON.parse(e);if(t!==null){const c=t.length;n.insertAdjacentHTML("beforeend",S(p())),a.textContent=c,h.textContent=k(t);return}a.textContent=0,y.innerHTML=v}r();n.addEventListener("click",f);function f(e){n.innerHTML="";const c=e.target.closest(".product-cart").dataset.id;i(c);const o=localStorage.getItem("basket"),s=JSON.parse(o);console.log(s.length),s.length===0&&u(),r()}b.addEventListener("click",P);function P(){u(),r()}function k(e){let t=0;return e.map(({price:c})=>t+=c),t.toFixed(2)}function S(e){return e.map(({_id:t,img:c,name:o,category:s,size:d,price:l})=>`
+</div>`;function n(){const e=localStorage.getItem("basket"),t=JSON.parse(e);if(t!==null){const o=t.length;r.insertAdjacentHTML("beforeend",x(d())),a.textContent=o,v.textContent=$(t);return}a.textContent=0,b.innerHTML=k}n();r.addEventListener("click",q);function q(e){r.innerHTML="";const o=e.target.closest(".product-cart").dataset.id;m(o);const s=localStorage.getItem("basket"),c=JSON.parse(s);console.log(c.length),c.length===0&&u(),n()}f.addEventListener("click",i);function i(){u(),n()}function $(e){let t=0;return e.map(({price:o})=>t+=o),t.toFixed(2)}function x(e){return e.map(({_id:t,img:o,name:s,category:c,size:l,price:p})=>`
       <li class="product-cart" data-id="${t}">
       <img
         class="product-picture"
-        src="${c}"
+        src="${o}"
         width="100"
         height="100"
       alt="avocado"
       />
       <div class="">
-        <p class="product-name">${o}</p>
+        <p class="product-name">${s}</p>
 
         <div class="product-descr">
-          <p>Category: <span class="qwe">${s}</span></p>
-          <p>Size: <span class="qwe">${d}</span></p>
+          <p>Category: <span class="qwe">${c}</span></p>
+          <p>Size: <span class="qwe">${l}</span></p>
         </div>
 
-        <p class="product-price">$${l}</p>
+        <p class="product-price">$${p}</p>
       </div>
       <button class="cart-btn" type="button">
         <svg class="">
-          <use href="${m}#icon-cross"></use>
+          <use href="${g}#icon-cross"></use>
         </svg>
       </button>
     </li>
-    `).join("")}
+    `).join("")}S.addEventListener("submit",L);function L(e){e.preventDefault();const t=d(),o=C(t);h.post(P,{email:e.target.elements.email.value,products:o}).then(s=>{s.status===201&&(i(),toggleModalShCart())}).catch(s=>console.log(s.message))}function C(e){return e.map(t=>({productId:t._id,amount:1}))}
 //# sourceMappingURL=commonHelpers2.js.map
